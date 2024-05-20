f4b0e4f17a809cbffbb36d48d8dc7c1b10da85a1
achyutbenz19 <achyut.benz@gmail.com>
2024-05-19 22:56:30 -0500

    css

--- components/talk.tsx
+++ components/talk.tsx
@@ -5,7 +5,7 @@ const Talk = () => {
   return (
     <div className="h-[650px] items-center justify-center flex">
       <div className="flex h-full items-end">
-        <Button className="rounded-full">
+        <Button className="py-7 rounded-full">
           <Mic />
         </Button>
       </div>
