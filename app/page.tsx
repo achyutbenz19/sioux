420d06c120dd81dea00c62e8c1116d09b1f4f638
achyutbenz19 <achyut.benz@gmail.com>
2024-05-22 13:19:40 -0500

    modes

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ const Home = () => {
   return (
     <main className="flex p-6 flex-col items-center md:mt-16 mt-10 justify-center">
       <Tabs
-        defaultValue="Draw"
+        defaultValue="Talk"
         className="w-full sm:w-[80%] mt-8 text-center space-y-6"
       >
         <TabsList className="w-full">
-          <TabsTrigger className="w-full" value="Draw">
-            Draw
-          </TabsTrigger>
           <TabsTrigger className="w-full" value="Talk">
             Talk
+          </TabsTrigger>
+          <TabsTrigger className="w-full" value="Draw">
+            Draw
           </TabsTrigger>
         </TabsList>
         <TabsContent value="Draw">
